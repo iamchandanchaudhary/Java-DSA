7a01c4d2a1fe4633c05d4b17c763d9a167439431
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-11 09:40:25 +0530

    Improvement 26

--- Practice/Practice 6.O (January - 2025)/Practice 302.java
+++ Practice/Practice 6.O (January - 2025)/Practice 302.java
@@ -5,8 +5,20 @@ class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number: ");
-        int number = sc.nextInt();
-        System.out.println("Number: " + number);
+        System.out.print("Enter 1st value: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter 2nd value: ");
+        int num2 = sc.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println("There Sum: " + sum);
+
+        if(sum % 2 == 0) {
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
     }
 }
