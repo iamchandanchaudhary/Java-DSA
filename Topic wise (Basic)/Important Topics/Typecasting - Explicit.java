a3ceb807f6d9d5719aee842a974cfaca52a6bcc4
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-12 07:40:53 +0530

    Improvement 27

--- Topic wise (Basic)/Important Topics/Typecasting - Explicit.java
+++ Topic wise (Basic)/Important Topics/Typecasting - Explicit.java
@@ -1,12 +1,13 @@
 class Typecasting {
     public static void main(String[] args) {
-        System.out.println("Typecasting - Implicit:- \n");
+        System.out.println("Typecasting - Explicit:- \n");
+        
+        double a = 27.5;
 
-        int a = 15;
-
-        double b = a;
+        int b = (int)a;
 
         System.out.println("A: " + a);
         System.out.println("B: " + b);
-    }
+    
+    }    
 }
