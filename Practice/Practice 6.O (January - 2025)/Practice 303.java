cd022e32593a72c2e7d8854408a07fee3f71050a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-02-15 14:11:28 +0530

    Improvement 29

---
+++ Practice/Practice 6.O (January - 2025)/Practice 303.java
@@ -0,0 +1,31 @@
+class Student {
+    String name;
+    int age;
+    int rollno;
+
+    public void getInfo() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+        System.out.println("Roll no.: " + this.rollno + "\n");
+    }
+}
+
+class Base {
+    public static void main(String[] args) {
+        System.out.println("Student Info.:- \n");
+        
+        Student s1 = new Student();
+        s1.name = "Chandan Chaudhary";
+        s1.age = 18;
+        s1.rollno = 230010;
+
+        s1.getInfo();
+
+        Student s2 = new Student();
+        s2.name = "Aman Verma";
+        s2.age = 21;
+        s2.rollno = 250010;
+
+        s2.getInfo();
+    }
+}
