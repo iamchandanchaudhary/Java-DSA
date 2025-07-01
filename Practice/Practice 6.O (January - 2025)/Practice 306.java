16b392222eb43cfce4dabd91714959d8e8876da7
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-01 07:40:33 +0530

    Improvement 40

---
+++ Practice/Practice 6.O (January - 2025)/Practice 306.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        int num = 5;
+
+        // ==> Square Pattern
+        // for(int i = 1; i <= num; i++) {
+        //     for(int j = 1; j <= num; j++) {
+        //         System.out.print("* ");
+        //     }
+        // System.out.println();
+        // }
+
+        // ==> Hollow Rectangle
+        // for(int i = 1; i <= num; i++) {
+        //     for(int j = 1; j <= num; j++) {
+        //         if(i == num || i == 1 || j == num || j == 1) {
+        //             System.out.print("* ");
+        //         }
+        //         else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        // System.out.println();
+        // }
+
+        // ==> Lower half pyramid
+        // for(int i = 1; i <= num; i++) {
+        //     for(int j = i; j <= num; j++) {
+        //         System.out.print("* ");
+        //     }
+        // System.out.println();
+        // }
+
+        // ==> Upper half pyramid
+        // for(int i = 1; i <= num; i++) {
+        //     for(int j = 1; j <= i; j++) {
+        //         System.out.print("* ");
+        //     }
+        // System.out.println();
+        // }
+
+        // ==> Upper half same number pyramid
+        // for(int i = 1; i <= num; i++) {
+        //     for(int j = 1; j <= i; j++) {
+        //         System.out.print(i + " ");
+        //     }
+        // System.out.println();
+        // }
+
+        // ==> Upper half different number pyramid
+        // for(int i = 1; i <= num; i++) {
+        //     for(int j = 1; j <= i; j++) {
+        //         System.out.print(j + " ");
+        //     }
+        // System.out.println();
+        // }
+
+        int n = 0;
+        // ==> Upper half different number pyramid
+        for(int i = 1; i <= num; i++) {
+            for(int j = 1; j <= i; j++) {
+                n = i;
+                System.out.print(n + " ");
+                n++;
+            }
+        System.out.println();
+        }
+    }
+}
