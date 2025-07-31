84b45d578b03d03db87ec5cf1bf2004a6fe4b4e5
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-07-31 22:46:00 +0530

    Improvement 54

---
+++ Practice/Practice 2.O/Pattern 5.java
@@ -0,0 +1,13 @@
+class FirstClass {
+    public static void main(String args[]) {
+        int n = 4;
+        int m = 5;
+
+        for(int i = 1; i <= n; i++ ) {
+            for( j = 1; j <= m; j++ ) {
+                System.out.println("*");
+            }
+        System.out.println();
+    }
+    }
+}
