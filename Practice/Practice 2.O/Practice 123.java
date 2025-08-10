e2b50bcd388b8f0936d0feef3180259c12b425e8
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-08-10 06:58:21 +0530

    Improvement 56

--- Practice/Practice 2.O/Practice 123.java
+++
@@ -1,9 +0,0 @@
-class FirstClass {
-    public static void main(String args[]) {
-
-        int number = 22;
-        int *ptr = &number;
-
-        System.out.println("Pointer is : " + ptr);
-    }
-}
