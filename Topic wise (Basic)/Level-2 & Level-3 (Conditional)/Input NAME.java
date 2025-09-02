32e4f36191f2620967207b0a5b70aa5eece3a60a
Chandan Chaudhary <chandanchaudhary533@gmail.com>
2025-09-02 20:12:51 +0530

    Improvement 60

--- Topic wise (Basic)/Level-2 & Level-3 (Conditional)/Input NAME.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class FirstClass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
-    }
-}
